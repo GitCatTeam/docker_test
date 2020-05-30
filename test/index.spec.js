0d7238d19361ea87a22087bfc8de55425f8cd1c0
Soyoung Kwon <onisley0424@gmail.com>
2020-05-30 23:31:07 +0900

    [TEST] TypeScript 테스트

--- test/index.spec.js
+++ test/index.spec.js
@@ -38,3 +38,24 @@ describe('Test Home API', function () {
         assert.equal('no', 'no');
     });
 });
+
+
+describe('Birthday API', function () {
+    /* 홈화면 API 테스트 */
+
+    it('사용자 이름을 주면 응답이 온다.', function () {
+        // null인 응답이 오면 안 된다.
+        // API.call(사용자이름);
+        assert.notEqual(null, true);
+    });
+
+    it('1 is 1', function () {
+        // 1은 1이어야 한다.
+        assert.equal(1, 1);
+    });
+
+    it('no is no', function () {
+        // 'no'는 'no'여야 한다.
+        assert.equal('no', 'no');
+    });
+});
